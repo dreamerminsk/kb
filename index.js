32e6d647a25adeede2c37de1d761bcb9c81d1b58
dreamerminsk <David70908019Miller>
2019-09-03 18:34:34 +0300

    kb day

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ const http = require('http');
 const axios = require('axios');
 const superagent = require('superagent');
 const cheerio = require('cheerio');
-var Datastore = require('nedb');
+const Datastore = require('nedb');
 
-var db = new Datastore({filename : 'users'});
-db.loadDatabase();
+var db = {};
+db.days = new Datastore({filename : 'days.nedb'});
+db.days.loadDatabase();
+db.days.ensureIndex({ fieldName: 'day', unique: true }, function (err) {
+});
 
-superagent.get('http://kinobusiness.com/kassovye_sbory/day/2019/2/')
+superagent.get('http://kinobusiness.com/kassovye_sbory/day/2019/9/')
 .end((err, res) => {
   if (err) { return console.log(err); }
   const $ = cheerio.load(res.text);
   $('table.calendar_year tbody tr td:first-child a').each(function() { 
+    console.log($(this).text());
     console.log($(this).attr('href'));
+	db.days.insert({'day': $(this).text(), 'ref': $(this).attr('href')});
   })
 });
+
+db.days.find({}).sort({ ref: 1 }).exec(function (err, docs) {
+  docs.forEach(function(doc){
+	  console.log(doc);
+  });
+});;
