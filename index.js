926ffc243955647163a90bc118bf5bc40652ada3
dKCh <tutmirage@gmail.com>
2019-09-03 14:39:21 +0300

    kb daily parser

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const fs = require('fs');
 const http = require('http');
 const axios = require('axios');
 const superagent = require('superagent');
-const cheerio = require('cheerio');
+const cheerio = require('cheerio')
 
-superagent.get('http://kinobusiness.com')
+superagent.get('http://kinobusiness.com/kassovye_sbory/day/2019/1/')
 .end((err, res) => {
   if (err) { return console.log(err); }
-  console.log(res.text);
+  const $ = cheerio.load(res.text);
+  $('table.calendar_year tbody tr').each(function() { 
+    console.log($(this).text());
+  })
 });
