ed59a82e788990687fbac12f27dead085334f17b
dKCh <tutmirage@gmail.com>
2019-09-03 15:16:28 +0300

    nedb

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const fs = require('fs');
 const http = require('http');
 const axios = require('axios');
 const superagent = require('superagent');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
+var Datastore = require('nedb');
 
-superagent.get('http://kinobusiness.com/kassovye_sbory/day/2019/1/')
+var db = new Datastore({filename : 'users'});
+db.loadDatabase();
+
+superagent.get('http://kinobusiness.com/kassovye_sbory/day/2019/2/')
 .end((err, res) => {
   if (err) { return console.log(err); }
   const $ = cheerio.load(res.text);
-  $('table.calendar_year tbody tr').each(function() { 
-    console.log($(this).text());
+  $('table.calendar_year tbody tr td:first-child a').each(function() { 
+    console.log($(this).attr('href'));
   })
 });
