eff789f61e87d8da5e13a2ef42dcf8552870f0e4
dreamerminsk <David70908019Miller>
2019-09-05 19:42:05 +0300

    koa app

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const http = require('http');
+//const axios = require('axios');
+const superagent = require('superagent');
+const cheerio = require('cheerio');
+const Datastore = require('nedb');
+
+var db = {};
+db.days = new Datastore({filename : 'days.nedb'});
+db.days.loadDatabase();
+db.days.ensureIndex({ fieldName: 'day', unique: true }, function (err) {
+});
+
+superagent.get('http://kinobusiness.com/kassovye_sbory/day/2019/9/')
+.end((err, res) => {
+  if (err) { return console.log(err); }
+  const $ = cheerio.load(res.text);
+  $('table.calendar_year tbody tr td:first-child a').each(function() { 
+    console.log($(this).text());
+    console.log($(this).attr('href'));
+	db.days.insert({'day': $(this).text(), 'ref': $(this).attr('href')});
+  })
+});
+
+db.days.find({}).sort({ ref: 1 }).exec(function (err, docs) {
+  docs.forEach(function(doc){
+	  console.log(doc);
+  });
+});;
