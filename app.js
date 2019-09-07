736dc844bc13f1dc4a14e697e37af4508339f3e8
dreamerminsk <David70908019Miller>
2019-09-07 15:31:41 +0300

    atlas

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const Koa = require('koa');
 const app = new Koa();
 
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kchureyna:111098chureyna@karocluster-bqhea.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 app.use(async (ctx, next) => {
   await next();
   const rt = ctx.response.get('X-Response-Time');
